aa90ae63e36ae0be4c8a91a708c9cb7119ffa631
Phi Luu <philectron@users.noreply.github.com>
2018-01-28 22:51:16 -0800

    Add Sum of F_m through F_n

---
+++ course1_algorithmic_toolbox/week2_algorithmic_warmup/fibonacci_partial_sum.cpp
@@ -0,0 +1,37 @@
+// File: fibonacci_partial_sum.cpp
+// Author: Phi Luu
+// Created: January 28, 2018
+//
+// Algorithmic Toolbox - Week 2 - Last Digit of the Sum of Fibonacci Numbers Again
+// See ./week2_algorithmic_warmup.pdf for details
+
+#include <iostream>
+
+int main(void) {
+    long long n, m;
+
+    std::cin >> m >> n;
+
+    // sum of all n fibonacci mod 10 = sum of each mod 10 fib
+    // = sum of all digits of a mod10 pisano period
+    // len(pisano period of 10) = 60
+    int pisano[60];
+    int sum = 0;
+    pisano[0] = 0;
+    pisano[1] = 1;
+
+    // calculate and store the pisano period
+    for (int i = 2; i < 60; i++) {
+        pisano[i] = (pisano[i - 2] + pisano[i - 1]) % 10;
+    }
+
+    // calculate the sum
+    for (int i = m; i < (n + 1) % 60; i++) {
+        sum = (sum + pisano[i]) % 10;
+    }
+
+    std::cout << sum << std::endl;
+
+
+    return 0;
+}
