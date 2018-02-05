978ec367e09a4c5112d84373b980948e389eed06
Phi Luu <philectron@users.noreply.github.com>
2018-02-05 13:56:44 -0800

    Complete Maximum Number of Prizes problem
    The key is to start with 1 and work the way up. Don't forget all the summands must be distinct from each other. Subtract the remaining sum until zero or until reach the number where it cannot be expressed by two summands (due to repeating terms). If in the case of repeating terms, take final unexpressable sum as the final summand.

--- course1_algorithmic_toolbox/week3_greedy_algorithms/different_summands.cpp
+++ course1_algorithmic_toolbox/week3_greedy_algorithms/different_summands.cpp
@@ -1,14 +1,37 @@
 // File: different_summands.cpp
 // Author: Phi Luu
-// Created: February 04, 2018
+// Created: February 05, 2018
 //
 // Algorithmic Toolbox - Week 03 - Greedy Algorithms
 // See ./week3_greedy_algorithms.pdf for details
 
 #include <iostream>
+#include <vector>
 
 int main(void) {
+    int n;
 
+    std::cin >> n;
+
+    std::vector<int> summand;
+    int candies = n;
+    // always start with 1 and work the way up to maximize number of summands
+    int i = 1;
+
+    // only add to summand if the subtraction is greater than the greatest summands
+    while ((candies - i > i) || (candies == i)) {
+        summand.push_back(i);  // record the summand
+        candies -= i;  // update the remaining candies
+        i++;  // try with the next integer
+    }
+
+    // if the candies is unable to be zero due to repeating summands, take the rest
+    if (candies > 0) summand.push_back(candies);
+
+    // output the maximum number of summands the list of them
+    std::cout << summand.size() << std::endl;
+    for (auto& num : summand) std::cout << num << " ";
+    std::cout << std::endl;
 
     return 0;
 }
