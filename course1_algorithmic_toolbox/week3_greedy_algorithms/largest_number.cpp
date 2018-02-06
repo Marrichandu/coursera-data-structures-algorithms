1dcc49c24f8655ff68baa99a1e1acfca6cf2120d
Phi Luu <philectron@users.noreply.github.com>
2018-02-05 18:41:40 -0800

    Complete Maximum Salary problem
    Given the two digit groups x and y, x is then put before y in the array only if the number formed when concatenate x and y is strictly greater than the number formed when concatenate y and x.

--- course1_algorithmic_toolbox/week3_greedy_algorithms/largest_number.cpp
+++ course1_algorithmic_toolbox/week3_greedy_algorithms/largest_number.cpp
@@ -1,14 +1,39 @@
 // File: largest_number.cpp
 // Author: Phi Luu
-// Created: February 04, 2018
+// Created: February 05, 2018
 //
 // Algorithmic Toolbox - Week 03 - Greedy Algorithms
 // See ./week3_greedy_algorithms.pdf for details
 
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector>
+
+bool XBeforeY(std::string x, std::string y);
 
 int main(void) {
+    int n, num;
 
+    std::cin >> n;
+
+    std::vector<std::string> digits(n);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> num;
+        digits[i] = std::to_string(num);
+    }
+
+    std::sort(digits.begin(), digits.end(), XBeforeY);
+
+    for (auto& digitgroup : digits) std::cout << digitgroup;
+
+    std::cout << std::endl;
 
     return 0;
 }
+
+// Concatenates two digit-only strings together.
+// If x + y > y + x, then x should be before y to maximize integer value -> true
+// Otherwise, returns false.
+bool XBeforeY(std::string x, std::string y) { return x + y > y + x; }
