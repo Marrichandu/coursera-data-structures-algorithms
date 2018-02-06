5bf1b73af3043670bc54f22d9a8bec5e80b08ad6
Phi Luu <philectron@users.noreply.github.com>
2018-02-05 23:57:50 -0800

    Init week 4

--- course1_algorithmic_toolbox/week4_divide_and_conquer/binary_search.cpp
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/binary_search.cpp
@@ -2,7 +2,7 @@
 // Author: Phi Luu
 // Created: February 05, 2018
 //
-// Algorithmic Toolbox - Week 4 - Divide and Conquer
+// Algorithmic Toolbox - Week 4 - Divide-and-Conquer Algorithms
 // See ./week4_divide_and_conquer.pdf for details
 
 #include <iostream>
