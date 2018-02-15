7b92308c385b2ffe27e4f07e5a0c46a363ff496f
Phi Luu <philectron@users.noreply.github.com>
2018-02-14 21:57:00 -0800

    Complete Improve Quicksort problem
    Improve quicksort on arrays with multiple recurrences using 3-way partition.

--- course1_algorithmic_toolbox/week4_divide_and_conquer/sorting.cpp
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/sorting.cpp
@@ -18,27 +18,12 @@ void QuickSort(std::vector<int>& array);
 void Swap(int* a, int* b);
 
 int main(void) {
-    // int n;
+    int n;
 
-    // std::cin >> n;
-    // std::vector<int> a(n);
-    // for (int i = 0; i < n; i++) std::cin >> a[i];
+    std::cin >> n;
 
-    std::vector<int> a;
-    a.push_back(2);
-    a.push_back(1);
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(2);
-    a.push_back(2);
-    a.push_back(4);
-    a.push_back(1);
-    a.push_back(2);
-    a.push_back(4);
-    a.push_back(9);
-    a.push_back(3);
-    a.push_back(4);
-    a.push_back(4);
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) std::cin >> a[i];
 
     QuickSort(a);
 
@@ -55,13 +40,8 @@ int main(void) {
 void Partition(std::vector<int>& array, int low, int high, int& i, int& j) {
     // handle 2-or-less-elements case
     if (high - low <= 2) {
-        std::cout << "2 OR LESS " << std::endl;
         // only swap if out of order
-        if (array[low] > array[high - 1]) {
-            Swap(&array[low], &array[high - 1]);
-            std::cout << "indices " << low << " and " << high - 1 << std::endl;
-        }
-
+        if (array[low] > array[high - 1]) Swap(&array[low], &array[high - 1]);
 
         // modify outputs and return
         i = low;
@@ -74,22 +54,10 @@ void Partition(std::vector<int>& array, int low, int high, int& i, int& j) {
     // get  mid  to after the last occurrence of pivot
     int pivot = array[high - 1], mid = low;
 
-
     while (mid < high) {
-        std::cout << "Pivot = " << pivot << "   ";
-        std::cout << "Low = " << low << "   ";
-        std::cout << "Mid = " << mid << "   ";
-        std::cout << "High = " << high << std::endl;;
-
-        for (int k = 0; k < array.size(); k++) std::cout << k % 10 << " "; std::cout << std::endl; for (auto& num : array) std::cout << num << " ";
-        std::cout << std::endl << std::endl;
-        std::cin >> c;
-        while (c != '\'') std::cin >> c;
-
         if (array[mid] < pivot) {
             // every element smaller than pivot must be put before  low
             Swap(&array[mid], &array[low]);
-            std::cout << "indices " << mid << " and " << low << std::endl;
             mid++;
             low++;
         } else if (array[mid] == pivot) {
@@ -98,40 +66,24 @@ void Partition(std::vector<int>& array, int low, int high, int& i, int& j) {
         } else {
             // every element greater than pivot must be put after  mid
             Swap(&array[mid], &array[high - 1]);
-            std::cout << "indices " << mid << " and " << high - 1 << std::endl;
             high--;
         }
     }
 
-
     // modify outputs and finish
     i = low;
     j = mid;
-
-    std::cout << "i = " << i << "   j = " << j << std::endl;
 }
 
 // Quicksorts the array from index  low  to index  high .
 void QuickSort(std::vector<int>& array, int low, int high) {
-    std::cout << "minlow = " << low << "   maxhigh = " << high << std::endl;
     // safely return if array has 1 element or less
     if (low >= high - 1) return;
 
     // chose a random pivot
     srand((unsigned int)time(nullptr));
     int pivotindex = rand() % (high - low) + low;
-
-    std::cout << "Pivot index = " << pivotindex << std::endl;
-
-    for (int k = 0; k < array.size(); k++) std::cout << k % 10 << " "; std::cout << std::endl; for (auto& num : array) std::cout << num << " ";
-    std::cout << std::endl;
-    std::cin >> c;
-    while (c != '\'') std::cin >> c;
-
-    std::cout << " NEW PARTITION" << std::endl;
-
     Swap(&array[high - 1], &array[pivotindex]);
-    std::cout << "indices " << high - 1 << " and " << pivotindex << std::endl;
 
     // partition the array by 3 ways and set values for onethid and twothird
     int onethird, twothird;
@@ -148,6 +100,4 @@ void Swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
-
-    std::cout << "Swapped ";
 }
