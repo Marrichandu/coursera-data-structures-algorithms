6940abfefb447a4ba57db4988fe12af2bf92b856
Phi Luu <philectron@users.noreply.github.com>
2018-02-19 14:37:29 -0800

    A better solution but still exceeds time limit

--- course1_algorithmic_toolbox/week4_divide_and_conquer/points_and_segments.cpp
+++ course1_algorithmic_toolbox/week4_divide_and_conquer/points_and_segments.cpp
@@ -68,8 +68,6 @@ int main(void) {
 
     //         if (naive == self) {
     //             std::cout << "    PASSED" << std::endl;
-    //             std::cout << "        naive = " << naive << std::endl;
-    //             std::cout << "        self  = " << self << std::endl;
     //         } else {
     //             std::cout << "    FAILED" << std::endl;
     //             std::cout << "        naive = " << naive << std::endl;
@@ -109,25 +107,14 @@ int SegmentsContainPoint(int point, std::vector<seg>& segment) {
     // find the index of the first segment that has greater left-end than point
     if (point <= segment.back().a) {
         int low = 0, high = segment.size();
-        bool found = false;
         while (low < high) {
             int mid = (low + high) / 2;
 
             if (point >= segment[mid].a) {
-                if (found) {
-                    for (int i = mid; i <= right; i++) {
-                        if ((i < right) && (point < segment[i].a)) {
-                            right = i;
-                            break;
-                        }
-                    }
-                    break;
-                }
                 low = mid + 1;
             } else {
                 right = mid;
                 high = mid;
-                found = true;
             }
         }
     }
@@ -143,22 +130,14 @@ int SegmentsContainPoint(int point, std::vector<seg>& segment) {
     // find the index of the first segment that has smaller right-end than point
     if (point >= segment.front().b) {
         int low = 0, high = right;
-        bool found = false;
         while (low < high) {
             int mid = (low + high) / 2;
 
             if (point <= segment[mid].b) {
-                if (found) {
-                    for (int i = left; i <= mid; i++) {
-                        if ((i < mid) && (point > segment[i].b)) left = i + 1;
-                    }
-                    break;
-                }
                 high = mid;
             } else {
                 left = mid + 1;
                 low = mid + 1;
-                found = true;
             }
         }
     }
